470fb8e6489f1bc1a33e6a940dfe848df1561797
hyeamykim <39967211+hyeamykim@users.noreply.github.com>
2018-11-01 08:25:39 +0100

    Update wine_reviews.py

--- wine_reviews.py
+++ wine_reviews.py
@@ -1,9 +1,6 @@
 
 # coding: utf-8
 
-# In[57]:
-
-
 """
 disclaimer: The following codes were studied and used for this task
 
@@ -22,10 +19,6 @@ By: Hye Yeon Kim
 
 
 # Part 1: Cleaning and preparing the data
-
-# In[58]:
-
-
 # importing necessary libraries
 
 import pandas as pd
@@ -48,47 +41,23 @@ import seaborn as sns
 
 get_ipython().magic('matplotlib inline')
 
-
-# In[59]:
-
-
 # importing data
 data = pd.read_csv('wine-reviews\winemag-data-130k-v2.csv')
 
-
-# In[60]:
-
-
 # checking how data looks like
 data.head()
-
-
-# In[61]:
-
 
 # dropping unnecessary unnamed columns
 data=data.drop('Unnamed: 0', axis=1)
 data=data.reset_index(drop=True)
 
-
-# In[62]:
-
-
 # checking for duplicates
 print("Total number of examples: ", data.shape[0])
 print("Number of examples with the same title and description: ", data[data.duplicated(['description','title'])].shape[0])
 
-
-# In[63]:
-
-
 # dropping duplicate wine reviews
 data=data.drop_duplicates(['description','title'])
 data=data.reset_index(drop=True)
-
-
-# In[64]:
-
 
 # checking how much of data is missing
 total = data.isnull().sum().sort_values(ascending = False)
@@ -96,40 +65,20 @@ percent = (data.isnull().sum()/data.isnull().count()*100).sort_values(ascending 
 missing  = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
 missing
 
-
-# In[65]:
-
-
 # dropping NA values in our target, taster_name
 data=data.dropna(subset=['taster_name'])
 data=data.reset_index(drop=True)
 
 
-# Part 2: (a quick) Exploratory Data Analysis
-
-# In[66]:
-
-
+# Part 2: Exploratory Data Analysis
 # adding description length in the data
 data['length'] = data['description'].apply(len)
-
-
-# In[67]:
-
 
 # checking the distrbution of description length
 data['length'].plot(bins=50, kind='hist')
 
-
-# In[68]:
-
-
 # checking if there is visible difference of description length with each taster
 data.hist(column='length', by='taster_name', bins=50, figsize=(10,4))
-
-
-# In[69]:
-
 
 # checking how many descriptions are there per each taster
 fig = plt.figure(figsize=(8,6))
@@ -137,10 +86,6 @@ data.groupby('taster_name').description.count().plot.bar(ylim=0)
 
 
 # Part 3: Natural Language Processing
-
-# In[70]:
-
-
 # preparing data for training by creating a smaller dataframe and by giving id to each taster
 
 from io import StringIO
@@ -161,10 +106,6 @@ id_to_taster = dict(taster_id_df[['taster_id', 'taster_name']].values)
 # checking how it looks
 data.head()
 
-
-# In[71]:
-
-
 # importing more libraries for language processing
 
 from nltk.stem.porter import PorterStemmer
@@ -172,10 +113,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from wordcloud import WordCloud, STOPWORDS
 import re
 from nltk.tokenize import RegexpTokenizer
-
-
-# In[72]:
-
 
 # for text preprocessing
 
@@ -195,76 +132,40 @@ def text_process(mess):
     # Now just remove any stopwords
     return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
 
-
-# In[73]:
-
-
 # checking if text_process works
 data.description.head(5).apply(text_process)
 
 
 # Part 4: Training and Testing the data
-
-# In[74]:
-
-
 # creating a pipeline to fit data
 pipeline = Pipeline([('bow', CountVectorizer(analyzer=text_process)),
                     ('tfidf', TfidfTransformer()),
                      ('classifier', MultinomialNB())                 
                     ])
 
-
-# In[75]:
-
-
 # splitting the data for training set and test set
 dat_train, dat_test, name_train, name_test = train_test_split(data['description'], data['taster_name'], test_size =0.2)
-
-
-# In[76]:
-
 
 # training the model
 pipeline.fit(dat_train, name_train)
 
-
-# In[77]:
-
-
 # predicting with the model
 predictions = pipeline.predict(dat_test)
-
-
-# In[78]:
-
 
 # prediction results
 print(classification_report(predictions, name_test))
 
 
 # Part 5: Different approaches of training and testing the data
-
-# In[79]:
-
-
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from catboost import Pool, CatBoostRegressor, cv
-
-
-# In[80]:
-
 
 # feature extraction for further usage of different models
 vect = TfidfVectorizer(analyzer='word', stop_words ='english', ngram_range=(1, 2), token_pattern=r'\w+',max_features=500)
 features = vect.fit_transform(data['description']).toarray()
 labels = data.taster_id
 features.shape
-
-
-# In[81]:
-
 
 # checking related features for each taster
 from sklearn.feature_selection import chi2
@@ -279,10 +180,6 @@ for taster_name, taster_id in sorted(taster_to_id.items()):
   print("# '{}':".format(taster_name))
   print("  . Most correlated unigrams:\n. {}".format('\n. '.join(unigrams[-N:])))
   print("  . Most correlated bigrams:\n. {}".format('\n. '.join(bigrams[-N:])))
-
-
-# In[82]:
-
 
 # preparing to compare different models
 
@@ -301,10 +198,6 @@ CV = 5
 cv_df = pd.DataFrame(index=range(CV * len(models)))
 entries = []
 
-
-# In[83]:
-
-
 # implementing different models and appending the accuracy results
 for model in models:
   model_name = model.__class__.__name__
@@ -312,17 +205,9 @@ for model in models:
   for fold_idx, accuracy in enumerate(accuracies):
     entries.append((model_name, fold_idx, accuracy))
 
-
-# In[84]:
-
-
 # creating a dataframe of accuracy of different models used
 cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
 cv_df.head()
-
-
-# In[85]:
-
 
 # visualizing the accuracy of different models
 import seaborn as sns
@@ -331,16 +216,8 @@ sns.stripplot(x='model_name', y='accuracy', data=cv_df,
               size=8, jitter=True, edgecolor="gray", linewidth=2)
 plt.show()
 
-
-# In[86]:
-
-
 # comparing accuracy of prediction results of different models
 cv_df.groupby('model_name').accuracy.mean()
-
-
-# In[87]:
-
 
 # visualizing confusion matrix to check the prediction results
 model = LinearSVC()
